5f7ad7b9732e39ff2c910ea1e3fabad06661bea2
Adam W <Code.AdamW78@gmail.com>
2022-10-13 15:49:32 -0400

    Wrote DictReader.py

--- SMSTexter/DictReader.py
+++ SMSTexter/DictReader.py
@@ -1,14 +1,20 @@
 import csv
 
 
-def read(filename):
+def read(filename) -> dict:
     with open(filename, newline='\n') as csvfile:
-        dict_reader = csv.reader(filename, delimiter=',')
+        carrier_dict_reader = csv.reader(csvfile, delimiter=',')
+        carrier_dict_list = list(carrier_dict_reader)
         carrier_dictionary = dict()
-        for row in dict_reader:
-            if row == f"{filename}.csv":
+        for carrier_dict_entry_line in carrier_dict_list:
+            # Skip first two lines of file
+            if carrier_dict_entry_line[0] == 'Cell Carrier' or carrier_dict_entry_line[0] == '':
                 continue
-            print()
+            email_list = list(carrier_dict_entry_line)
+            # Remove Cell Carrier from list and store
+            carrier = email_list.pop(0)
+            # Remove blank entries
+            formatted_email_list = [email for email in email_list if email != '']
+            carrier_dictionary[carrier] = formatted_email_list
+        return carrier_dictionary
 
-
-read("carrierdict.csv")
