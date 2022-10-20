ba41e0081ecc5878962929a9a31f936718ecfcaf
Adam W <Code.AdamW78@gmail.com>
2022-10-20 14:03:32 -0400

    Random testing

--- data-types.py
+++ data-types.py
@@ -1,3 +1,13 @@
 typeArr = [True, "str", 1, 1.0]
 for var in typeArr:
     print(f"This variable has a type of {type(var)} and a value of {var}.")
+num = [29, 4, 6, 2, 1, 19, 67]
+num2 = num[2:3]
+print(num2)
+s1 = "New York"
+s2 = "Boston"
+even = 31 % 2 == 0
+if even = True:
+    print("it is even!")
+else:
+    print("it is odd!")
